aa7abbab57886fb0437f1c608a2840dfc3a8bd96
KolaSiro <kurt.muellner@bluewin.ch>
2014-07-04 22:25:07 +0200

    Bla

---
+++ src/application/SampleController.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SampleController implements Initializable
+{
+	@FXML
+	private Label lblText;
+	
+	@FXML
+	private Button btnHallo;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
