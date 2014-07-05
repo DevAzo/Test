8393b00d4aa019f8143f9722bc2129263fbe9119
KolaSiro <kurt.muellner@bluewin.ch>
2014-07-05 12:22:13 +0200

    Fies renamed, Classes renamed

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,14 +10,28 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/Sample.fxml"));
+	public void start(Stage primaryStage) 
+	{
+		try 
+		{
+			// Das ist der Controller der View
+			// Wie die View aussieht, steht als XML-Datei in der Datei Sample.fxml
+			Parent root = FXMLLoader.load(getClass().getResource("/application/View.fxml"));
+			
+			// Das ist die View
 			Scene scene = new Scene(root);
+			
+			// Das ist das Style Sheet für die View
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Das Main mit der ersten View verbinden.
 			primaryStage.setScene(scene);
+			
+			// Erste View anzeigen, ab hier geht es los.
 			primaryStage.show();
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
---
+++ src/application/ViewController.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+// Kommentar
+public class ViewController implements Initializable
+{
+	@FXML
+	private Label lblText;
+	
+	@FXML
+	private Button btnHallo;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) 
+	{
+		
+	}	
+
+	public void onClick(ActionEvent event)
+	{
+		lblText.setText("Ich bin der neue Text");
+	}
+
+}
