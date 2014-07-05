281050b70388bb40d4e85f02f1347aca7864a973
KolaSiro <kurt.muellner@bluewin.ch>
2014-07-05 11:18:56 +0200

    GUI erweitert.
    Label gibt nun Text aus, wenn auf Button geklickt.

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -3,6 +3,7 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -17,8 +18,15 @@ public class SampleController implements Initializable
 	private Button btnHallo;
 
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+	public void initialize(URL arg0, ResourceBundle arg1) 
+	{
 		
 	}
+	
+
+	public void onClick(ActionEvent event)
+	{
+		lblText.setText("Ich bin der neue Text");
+	}
+
 }
